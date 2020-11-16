c8eb9185e67aede0f301fbbee901a3b4473ce7fd
Kevin Martinez <kamgmc@gmail.com>
2020-11-16 09:33:13 -0400

    Backend structure

---
+++ backend/src/main/java/io/ruke/backend/demo/BackendDemoApplication.java
@@ -0,0 +1,13 @@
+package io.ruke.backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/ruke/backend/demo/BackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ruke.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
